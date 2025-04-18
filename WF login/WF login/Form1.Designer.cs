022e495580731fb4db7fcb9268bca041664adb15
KAINABRUMHABEKOST <kainabrum123@gmail.com>
2025-04-17 21:57:57 -0300

    atividade de csss

--- WF login/WF login/Form1.Designer.cs
+++ WF login/WF login/Form1.Designer.cs
@@ -28,12 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Blue;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(299, 450);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.InitialImage = (Image)resources.GetObject("pictureBox1.InitialImage");
+            pictureBox1.Location = new Point(39, 101);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(221, 228);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Black;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.ForeColor = Color.Gray;
+            textBox1.Location = new Point(357, 121);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(308, 24);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Black;
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(357, 257);
+            button1.Name = "button1";
+            button1.Size = new Size(308, 34);
+            button1.TabIndex = 3;
+            button1.Text = "Cadastre - se";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(464, 337);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(159, 25);
+            linkLabel1.TabIndex = 0;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Esqueceu a senha?";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(652, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(78, 52);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(724, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(83, 52);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.Black;
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.ForeColor = Color.Gray;
+            textBox3.Location = new Point(357, 193);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(308, 24);
+            textBox3.TabIndex = 2;
+            textBox3.Text = "Senha:";
+            textBox3.Enter += textBox3_Enter;
+            textBox3.Leave += textBox3_Leave;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.Black;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.ForeColor = Color.Gray;
+            textBox2.Location = new Point(357, 121);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(308, 24);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "Usuário:";
+            textBox2.Enter += textBox2_Enter_1;
+            textBox2.Leave += textBox2_Leave;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(357, 138);
+            label1.Name = "label1";
+            label1.Size = new Size(362, 25);
+            label1.TabIndex = 9;
+            label1.Text = "__________________________________________________";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(357, 208);
+            label2.Name = "label2";
+            label2.Size = new Size(362, 25);
+            label2.TabIndex = 10;
+            label2.Text = "__________________________________________________";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(814, 450);
+            Controls.Add(textBox2);
+            Controls.Add(textBox3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(linkLabel1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(panel1);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private Button button1;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
     }
 }
--- WF login/WF login/Form1.cs
+++ WF login/WF login/Form1.cs
@@ -2,9 +2,65 @@ namespace WF_login
 {
     public partial class Form1 : Form
     {
+        public object Aplication { get; private set; }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+
+
+
+
+
+        private void textBox3_Enter(object sender, EventArgs e)
+        {
+            textBox3.Text = string.Empty;
+
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            textBox3.Text = "Senha:";
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            var resposta = MessageBox.Show("você clicou no botão 2",
+              "Sair",
+              MessageBoxButtons.YesNo,
+              MessageBoxIcon.Question);
+            if (resposta == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == string.Empty)
+                textBox2.Text = "Usuário:";
+        }
+
+        private void textBox2_Enter_1(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "Usuário:")
+                textBox2.Text = string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var formPrincipal = new FormPrincipal();
+            formPrincipal.Show();
         }
     }
 }
